ea15dd6b81484733d258bc3aae920b949a690eff
Musaib <musaib709@outlook.com>
2024-10-10 21:25:29 -0700

    updated baseurl

--- front-end/src/app/environments/environment.ts
+++ front-end/src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'http://localhost:5000',
+  // apiBaseUrl: 'http://localhost:5000',
+  apiBaseUrl: 'https://farmers-dry-fruit-server.onrender.com',
   carouselCategory: '66fae15bb93cfc70328da2ad',
 };
