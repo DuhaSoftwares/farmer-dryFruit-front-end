6f7a141020488b1b100fbbcb03fd781e4ba31db3
Musaib <musaib709@outlook.com>
2024-10-23 11:30:06 -0700

    updated

--- js/cart.js
+++ js/cart.js
@@ -138,7 +138,7 @@ $(".menu-items a").click(function () {
 
 // Fetch data from db.json
 // Fetch data from db.json
-fetch("./newArivals.json")
+fetch("https://duhasoftwares.github.io/newArivals.json")
   .then((response) => response.json())
   .then((data) => {
     populateProducts(data.newArrivals, "#new-arrivals .best-seller");
